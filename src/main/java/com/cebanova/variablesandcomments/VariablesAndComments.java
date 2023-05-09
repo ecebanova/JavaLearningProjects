95a45e9eee08f595db4fb1a653db3d82ad69b3d1
ecebanova <elena.cebanova97@gmail.com>
2023-05-09 22:48:48 +0300

    Added the Variables and Comments Task.

---
+++ src/main/java/com/cebanova/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,67 @@
+package com.cebanova.variablesandcomments;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+
+//        Am declarat toate tipurile de variabile primitive
+
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimit0fByteDataType;
+        byte upperLimit0fByteDataType;
+        short lowerLimit0fShortDataType;
+        short upperLimit0fShortDataType;
+        int lowerLimit0fIntDataType;
+        int upperLimit0fIntDataType;
+        long lowerLimit0fLongDataType;
+        long upperLimit0fLongDataType;
+        float lowerLimit0fFloatDataType;
+        float upperLimit0fFloatDataType;
+        double lowerLimit0fDoubleDataType;
+        double upperLimit0fDoubleDataType;
+
+//      Am initializat variabilele de mai sus
+
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        firstCharFromAscii = 'a';
+        lastCharFromAscii = 'z';
+        lowerLimit0fByteDataType = -128;
+        upperLimit0fByteDataType = 127;
+        lowerLimit0fShortDataType = -32768;
+        upperLimit0fShortDataType = 32767;
+        lowerLimit0fIntDataType = -2147483648;
+        upperLimit0fIntDataType = 2147483647;
+        lowerLimit0fLongDataType = -9223372036854775808L;
+        upperLimit0fLongDataType = 9223372036854775807L;
+        lowerLimit0fFloatDataType = -3.4E+38f;
+        upperLimit0fFloatDataType = 3.4E+38f;
+        lowerLimit0fDoubleDataType = -1.7E+308d;
+        upperLimit0fDoubleDataType = 1.7E+305;
+
+//     Am afisat valoarea variabilelor la consola
+
+        System.out.println(falseBooleanVariable);
+        System.out.println(trueBooleanVariable);
+        System.out.println(firstCharFromAscii);
+        System.out.println(lastCharFromAscii);
+        System.out.println(lowerLimit0fByteDataType);
+        System.out.println(upperLimit0fByteDataType);
+        System.out.println(lowerLimit0fShortDataType);
+        System.out.println(upperLimit0fShortDataType);
+        System.out.println(lowerLimit0fIntDataType);
+        System.out.println(upperLimit0fIntDataType);
+        System.out.println(lowerLimit0fLongDataType);
+        System.out.println(upperLimit0fLongDataType);
+        System.out.println(lowerLimit0fFloatDataType);
+        System.out.println(upperLimit0fFloatDataType);
+        System.out.println(lowerLimit0fDoubleDataType);
+        System.out.println(upperLimit0fDoubleDataType);
+
+    }
+}
+
+
+
